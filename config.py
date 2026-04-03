0e1c71e2200cb2f44b7a40dd8706e0ea36a940cd
Valiyev Kamron <valiyevkamron201@gmail.com>
2026-04-03 20:08:04 +0500

    Change BOT_TOKEN to new environment variable
    Updated BOT_TOKEN to a new value.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # ── Main Bot ─────────────────────────────────────────
-BOT_TOKEN     = os.getenv("7810689974:AAEhQWKg14b1SwFEiuad-P2R8SMCFiusQbc", "")
+BOT_TOKEN     = os.getenv("7810689974:AAHbALRBHfRtEgvmbr-hFnNaE4CmG-3ImKM", "")
 ADMIN_IDS     = list(map(int, os.getenv("ADMIN_IDS", "6498632307").split(",")))
 
 # ── Telegram API ─────────────────────────────────────
